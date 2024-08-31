ae63248a8f5d2994639c8fa0faec56a7582f39e7
Wesley.R <wesleyribas2015@gmail.com>
2024-08-31 16:51:16 -0300

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,7 @@ export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3000),
   SONAR_TOKEN: z.string().optional(),
   DOCKERHUB_TOKEN: z.string().optional(),
-  DATABASE_URL: z.string().url({ message: "DATABASE_URL inválida" }),
+  DATABASE_URL: z.string().url({ message: "DATABASE_URL inválida" }).default("postgresql://user:password@localhost:5432/shopper?schema=public"),
   GEMINI_API_KEY: z.string().nonempty({ message: "GEMINI_API_KEY é obrigatório" }),
 });
 
